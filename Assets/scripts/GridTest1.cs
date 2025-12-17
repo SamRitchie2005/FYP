40d56afcc39b556729f03d524bfca39490d43314
SamRitchie2005 <samritchie2005@gmail.com>
2025-12-17 06:54:20 +0000

    automata

--- Assets/scripts/GridTest1.cs
+++ Assets/scripts/GridTest1.cs
@@ -12,87 +12,156 @@ public class GridTest1 : MonoBehaviour
     TilemapCollider2D tCollider;
     [SerializeField] int automatonLoops;
     int neighbourLive;
+    int[,] mapArray;
 
     private void Awake()
     {
+        mapArray = new int[100,100];
         tCollider = tilemapWall.GetComponent<TilemapCollider2D>();
         for (int i = 0; i < 100; i++)
         {
             for (int j = 0; j < 100; j++)
             {
                 int flip = (int)Mathf.Round(Mathf.PerlinNoise(seed+i+0.1f, seed+j+0.1f));
-                pos = new Vector3Int(i, j, 0);
+               // pos = new Vector3Int(i, j, 0);
 
                 // int flip = Random.Range(0, 2);
                 if (flip == 0)
                 {
-                    tilemapFloor.SetTile(pos, tileFloor);
+                    //tilemapFloor.SetTile(pos, tileFloor);
+                    mapArray[i,j] = 0;
                 }
                 else if (flip == 1)
                 {
-                    tilemapWall.SetTile(pos, tileWall);
+                    //tilemapWall.SetTile(pos, tileWall);
+                    mapArray[i, j] = 1;
                 }
-                for (int k = 0; k < automatonLoops; k++)
-                {
-                    CellularAutomaton();
-                }
+                //for (int k = 0; k < automatonLoops; k++)
+                //{
+                //   CellularAutomaton();
+                //}
      
             }
         }
-
+        for (int k = 0; k < automatonLoops; k++)
+        {
+           CellularAutomaton();
+        }
+        DrawMap();
         tCollider.enabled = false;
         tCollider.enabled = true;
 
     }
     void CellularAutomaton()
     {
-        for (int i = 45; i < 55; i++)
+        for (int i = 1; i < 99; i++)
         {
-            for (int j = 45; j < 55; j++)
+            for (int j = 1; j < 99; j++)
             {
+                //neighbourLive = 0;
+                //if (tilemapWall.GetTile(new Vector3Int(i-1, j-1, 0))==tileWall){
+                //    neighbourLive++;
+                //}
+                //if (tilemapWall.GetTile(new Vector3Int(i - 1, j , 0)) == tileWall)
+                //{
+                //    neighbourLive++;
+                //}
+                //if (tilemapWall.GetTile(new Vector3Int(i - 1, j + 1, 0)) == tileWall)
+                //{
+                //    neighbourLive++;
+                //}
+                //if (tilemapWall.GetTile(new Vector3Int(i , j - 1, 0)) == tileWall)
+                //{
+                //    neighbourLive++;
+                //}
+                //if (tilemapWall.GetTile(new Vector3Int(i , j + 1, 0)) == tileWall)
+                //{
+                //    neighbourLive++;
+                //}
+                //if (tilemapWall.GetTile(new Vector3Int(i + 1, j - 1, 0)) == tileWall)
+                //{
+                //    neighbourLive++;
+                //}
+                //if (tilemapWall.GetTile(new Vector3Int(i + 1, j , 0)) == tileWall)
+                //{
+                //    neighbourLive++;
+                //}
+                //if (tilemapWall.GetTile(new Vector3Int(i + 1, j + 1, 0)) == tileWall)
+                //{
+                //    neighbourLive++;
+                //}
+                //if (neighbourLive == 3) { 
+                //    tilemapWall.SetTile(new Vector3Int(i , j , 0), tileWall); 
+                //    tilemapFloor.SetTile(new Vector3Int(i, j, 0), null);
+                //}
+                //if (neighbourLive <1||neighbourLive>5)
+                //{
+                //    tilemapWall.SetTile(new Vector3Int(i, j, 0), null);
+                //    tilemapFloor.SetTile(new Vector3Int(i, j, 0), tileFloor);
                 neighbourLive = 0;
-                if (tilemapWall.GetTile(new Vector3Int(i-1, j-1, 0))==tileWall){
-                    neighbourLive++;
-                }
-                if (tilemapWall.GetTile(new Vector3Int(i - 1, j , 0)) == tileWall)
+                if (mapArray[i-1,j-1]==1)
                 {
                     neighbourLive++;
                 }
-                if (tilemapWall.GetTile(new Vector3Int(i - 1, j + 1, 0)) == tileWall)
+                if (mapArray[i, j-1] == 1)
                 {
                     neighbourLive++;
                 }
-                if (tilemapWall.GetTile(new Vector3Int(i , j - 1, 0)) == tileWall)
+                if (mapArray[i+1, j - 1] == 1)
                 {
                     neighbourLive++;
                 }
-                if (tilemapWall.GetTile(new Vector3Int(i , j + 1, 0)) == tileWall)
+                if (mapArray[i-1, j] == 1)
                 {
                     neighbourLive++;
                 }
-                if (tilemapWall.GetTile(new Vector3Int(i + 1, j - 1, 0)) == tileWall)
+                if (mapArray[i+1, j] == 1)
                 {
                     neighbourLive++;
                 }
-                if (tilemapWall.GetTile(new Vector3Int(i + 1, j , 0)) == tileWall)
+                if (mapArray[i - 1, j+1] == 1)
                 {
                     neighbourLive++;
                 }
-                if (tilemapWall.GetTile(new Vector3Int(i + 1, j + 1, 0)) == tileWall)
+                if (mapArray[i, j+1] == 1)
                 {
                     neighbourLive++;
                 }
-                if (neighbourLive == 3) { 
-                    tilemapWall.SetTile(new Vector3Int(i , j , 0), tileWall); 
-                    tilemapFloor.SetTile(new Vector3Int(i, j, 0), null);
-                }
-                if (neighbourLive <1||neighbourLive>5)
+                if (mapArray[i+1, j+1] == 1)
                 {
-                    tilemapWall.SetTile(new Vector3Int(i, j, 0), null);
-                    tilemapFloor.SetTile(new Vector3Int(i, j, 0), tileFloor);
+                    neighbourLive++;
+                }
+                if (neighbourLive == 3)
+                {
+                    mapArray[i, j] = 1;
+                }
+                if (neighbourLive < 1 || neighbourLive >4)
+                {
+                    mapArray[i, j] = 0;
                 }
             }
         }
     }
 
+
+    void DrawMap()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            for (int j = 0; j < 100; j++)
+            {
+                pos = new Vector3Int(i, j, 0);
+                if (mapArray[i,j] == 0)
+                {
+                    tilemapFloor.SetTile(pos, tileFloor);
+                    
+                }
+                else if (mapArray[i, j] == 1)
+                {
+                     tilemapWall.SetTile(pos, tileWall);
+                    
+                }
+            }
+        }
+    }
 }
